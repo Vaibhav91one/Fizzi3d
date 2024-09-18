db5f1bbb0222097cb1aaabb376796bd711ce3b92
Vaibhav91one <vtking03@gmail.com>
2024-09-18 19:22:37 +0530

    initial project

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FizziLogo } from './FizziLogo'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <>
+        <header className='flex justify-center py-4 -mb-28'>
+            <FizziLogo className='h-20 z-10 cursor-pointer text-sky-800'/>
+        </header>
+    </>
+)
+}
+
+export default Header
